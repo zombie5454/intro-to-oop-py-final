fa276a3c279f07d1a2134f2b0bc53a9b83a3d27c
moshiachi <eft580308@gmail.com>
2022-06-13 01:12:44 +0800

    question_bank 06/13

--- Model/question_bank.py
+++ Model/question_bank.py
@@ -1,4 +1,4 @@
-import os
+import os,re,random
 import pickle
 from typing import List
 
@@ -11,40 +11,97 @@ class QuestionBank:
         self.__name = name
         # directory name  == name 
         self.__directory = directory
-        self.__issuedID = -1
+        self.__issuedID = self.find_max()
         # TODO: find the maximum ID in the directory (ex: 1.pickle, 0.pickle, 3.pickle --> maxi ID is 3)
         
     #TODO: getter of __directory, __name, __issuedID
     #need setter for __name
+    @property
+    def directory(self):
+        return self.__directory
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, newname: str):
+        self.__name=newname
+    @property
+    def issuedID(self):
+        return self.__issuedID
+
+    def find_max(self) -> int:
+        list_files=os.listdir(self.__directory)
+        max_n = -1
+        for file in list_files:
+            num = int(re.search('(\d*).pickle', file).group(1))
+            max_n = num if num > max_n else max_n
+        return max_n
 
     def addQuestion(self, q: Question) -> bool:
         # change the q's ID to issuedID + 1
+        q_id=self.__issuedID+1
+        q.ID=q_id
+        # if file already exists, return false
+        if "{}.pickle".format(q_id) in os.listdir(self.__directory):
+            return False
+
         # TODO: use pickle to dump q in self.directory
         # pickle name = q's ID
-        # if file already exists, return false
+        path=self.__directory+"/{}.pickle".format(q_id)
+        with open(path,'wb') as f:
+            pickle.dump(q,f)
+
         return True
     
     def getQuestionList(self, num=-1)-> List[Question]:
         if num < 0:
             # TODO: append all question in self.directory
-            pass
+            q_list=[]
+            list_files=os.listdir(self.__directory)
+
+            for file in list_files:
+                path=self.__directory+"/"+file
+                with open(path,'rb') as f:
+                    x=pickle.load(f)
+                q_list.append(x)
+
         else:
             # TODO: pick random num of questions
-            pass
+            q_list=[]
+            list_files=random.sample(os.listdir(self.__directory), num)
+        
+            for file in list_files:
+                path=self.__directory+"/"+file
+                with open(path,'rb') as f:
+                    x=pickle.load(f)
+                q_list.append(x)
+            
     
         # shuffle the list, and return it
-        return []
+        random.shuffle(q_list)
+
+        return q_list
 
     def editQuestion(self, changed_question: Question)->bool:
         # TODO: use question object's ID to open that pickle in w/wb mode
         # DNE -> return false
+        ch_id=changed_question.ID
+        if "{}.pickle".format(ch_id) not in os.listdir(self.__directory):
+            return False
+        path=self.__directory+"/{}.pickle".format(ch_id)
+        with open(path,'wb') as f:
+            pickle.dump(changed_question,f)
         # overwrite the pickle with that objects
-        return False
+        return True
 
     def deleteQuestion(self, ID: int)->bool:
         # TODO: use ID to delete that pickle
+        if "{}.pickle".format(ID) not in os.listdir(self.__directory):
+            return False
         # DNE -> return false
-        return False
+        path=self.__directory+"/{}.pickle".format(ID)
+        os.remove(path)
+        return True
 
 if __name__ == "__main__":
     qBank = QuestionBank()
