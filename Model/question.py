d0dac20cc27ce0d93aebe6724dd9c1f8fe7806dd
moshiachi <eft580308@gmail.com>
2022-06-13 01:12:24 +0800

    question 06/13

--- Model/question.py
+++ Model/question.py
@@ -1,19 +1,42 @@
 # TODO: import anything that needed
 from typing import List
+from question_type import QuestionType
 
 class Question:
-    def __init__(self, type:str, question: str, ans) -> None:
+    def __init__(self, type:QuestionType, question: str) -> None:
         self.__type = type
         self.__question = question
         # for short answer, ans is str
         # for multiple choice and choice, ans is list of choice options
-        self.__ans = ans
+        #self.__ans = ans
+        #我把題目的ans放在sub class裡面
         self.__ID = -1
         return
     
     
     # TODO: getter and setter 
     # NO setter for ID
+    @property
+    def type(self):
+        return self.__type
+    @type.setter
+    def type(self, newtype: QuestionType):
+        self.__type = newtype
+    
+    @property
+    def question(self):
+        return self.__question
+    @type.setter
+    def question(self, newquestion: str):
+        self.__question = newquestion
+    
+    @property
+    def ID(self):
+        return self.__ID
+    @ID.setter
+    def ID(self, newID: int):
+        self.__ID = newID
+    
 
 
     def checkAnswer(answer: str) -> bool:
@@ -22,37 +45,68 @@ class Question:
     # short answer -> answer == ans?
     # choice: is ans[answer[i]].is_true == true?
     # multiple choice: is i in answer (ans[i].is_true == true)? is ans[answer[i]].is_true == true?
-        return True
+    #    return True
+    #我把checkAnswer放在subclass下面
+        pass
 
 class ChoiceOption:
-    def __init__(self, text="", is_true=False) -> None:
+    def __init__(self, text : str, is_true : bool):
         self.__text = text
         self.__is_true = is_true
-        pass
 
     # TODO: getter and setter
     @property
     def text(self) -> str:
         return self.__text
-    @text.setter
-    def text(self, newText: str) -> str:
-        self.__text = newText
     @property
     def is_true(self) -> str:
         return self.__is_true
-    @is_true.setter
-    def is_true(self, isAns: bool) -> str:
-        self.__is_true = isAns
     
 
 class ShortAnswer(Question):
-    def __init__(self, question: str, ans: str) -> None:
-        super().__init__("short_answer", question,ans)
+    def __init__(self, question: str, ans: str):
+        super().__init__(QuestionType.FILL, question)
+        self.__ans = ans
+    @property
+    def ans(self):
+        return self.__ans
     
+    def checkAnswer(self , answer: str) -> bool:
+        answer=answer.upper()
+        if answer.find(self.__ans.upper()) ==-1:
+            return False
+        else:
+            return True
+
 class MultipleChoice(Question):
-    def __init__(self, question: str, choice: List[ChoiceOption]) -> None:
-        super().__init__("multiple_choice", question, choice)
+    def __init__(self, question: str, choices: List[ChoiceOption]):
+        super().__init__(QuestionType.MULTIPLECHOICE, question)
+        self.__choices=choices
+    @property
+    def choices(self):
+        return self.__choices
+    
+    def checkAnswer(self , answer) -> bool:
+        #該answer成員為包含is_true、selected跟text的class
+        for i in answer:
+            if i.selected != i.is_true:
+                return False
+        return True
 
 class Choice(Question):
-    def __init__(self, question: str, choice: List[ChoiceOption]) -> None: 
-        super().__init__("choice", question, choice)
+    def __init__(self, question: str, choices: List[ChoiceOption]): 
+        super().__init__(QuestionType.CHOICE, question)
+        self.__choices=choices
+    @property
+    def choices(self):
+        return self.__choices
+
+    def checkAnswer(self , answer) -> bool:
+        #該answer成員為包含is_true、selected跟text的class
+        for i in answer:
+            if i.selected != i.is_true:
+                return False
+        return True
+
+
+
